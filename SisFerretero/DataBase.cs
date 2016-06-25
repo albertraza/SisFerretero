1553b9b76ed24440d7821b96d425b65762a727dc
albertraza <arbert.jr@gmail.com>
2016-06-25 08:16:18 -0400

    Add new method for getting Data base connection string

--- SisFerretero/DataBase.cs
+++ SisFerretero/DataBase.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data.SqlClient;
+using SisFerretero.Properties;
 
 namespace SisFerretero
 {
     public class DataBase
     {
-
+        // metodo para poder obtener y devolver la conexion de la base de datos.
+        public static SqlConnection connect()
+        {
+            SqlConnection con = new SqlConnection(Settings.Default.FerreteriaDBConnectionString);
+            con.Open();
+            return con;
+        }
     }
 }
