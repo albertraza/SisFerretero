180a0c6d60b327b87ce27819ac53586a5a6487da
albertraza <arbert.jr@gmail.com>
2016-07-08 11:04:02 -0400

    Add categoria class including the getter method

---
+++ SisFerretero/categoria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SisFerretero
+{
+    public class categoria
+    {
+        public int codigo { get; set; }
+        public string Categoria { get; set; }
+
+        public categoria() { }
+        public categoria(int c, string ca)
+        {
+            codigo = c;
+            Categoria = ca;
+        }
+
+        // metodo que obtiene la categoria
+        public static categoria getCategoria(string categoria)
+        {
+            categoria pCateforia = new categoria();
+            using(SqlConnection con = DataBase.connect())
+            {
+                SqlCommand comand = new SqlCommand(string.Format("select * from categorias where categoria = '{0}'", categoria), con);
+                SqlDataReader reader = comand.ExecuteReader();
+                if (reader.HasRows)
+                { 
+                    while (reader.Read())
+                    {
+                        pCateforia.codigo = Convert.ToInt32(reader["codigo"]);
+                        pCateforia.Categoria = reader["categoria"].ToString();
+                    }
+                }
+                else
+                {
+                    pCateforia = null;
+                }
+                con.Close();
+            }
+            return pCateforia;
+        }
+    }
+}
