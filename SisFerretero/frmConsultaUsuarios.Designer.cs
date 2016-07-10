3a3fed2a73f8c5daa7da5201e00370039ae25a45
albertraza <arbert.jr@gmail.com>
2016-07-09 23:19:56 -0400

    add form consulta usuarios

---
+++ SisFerretero/frmConsultaUsuarios.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace SisFerretero
+{
+    partial class frmConsultaUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsultaUsuarios));
+            this.pOcultar = new System.Windows.Forms.Panel();
+            this.lblOcultar = new System.Windows.Forms.LinkLabel();
+            this.pCerrar = new System.Windows.Forms.Panel();
+            this.lblCerrar = new System.Windows.Forms.LinkLabel();
+            this.gbFiltros = new System.Windows.Forms.GroupBox();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.lblFiltrarpor = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblDigiteSuBusqueda = new System.Windows.Forms.Label();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.btnNuevaBusqueda = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.pOcultar.SuspendLayout();
+            this.pCerrar.SuspendLayout();
+            this.gbFiltros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pOcultar
+            // 
+            this.pOcultar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pOcultar.Controls.Add(this.lblOcultar);
+            this.pOcultar.Location = new System.Drawing.Point(523, 12);
+            this.pOcultar.Name = "pOcultar";
+            this.pOcultar.Size = new System.Drawing.Size(81, 37);
+            this.pOcultar.TabIndex = 0;
+            this.pOcultar.Click += new System.EventHandler(this.pOcultar_Click);
+            this.pOcultar.MouseHover += new System.EventHandler(this.pOcultar_MouseHover);
+            // 
+            // lblOcultar
+            // 
+            this.lblOcultar.ActiveLinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.AutoSize = true;
+            this.lblOcultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOcultar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblOcultar.LinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.Location = new System.Drawing.Point(13, 11);
+            this.lblOcultar.Name = "lblOcultar";
+            this.lblOcultar.Size = new System.Drawing.Size(53, 15);
+            this.lblOcultar.TabIndex = 0;
+            this.lblOcultar.TabStop = true;
+            this.lblOcultar.Text = "Ocultar";
+            this.lblOcultar.VisitedLinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblOcultar_LinkClicked);
+            // 
+            // pCerrar
+            // 
+            this.pCerrar.BackColor = System.Drawing.Color.Red;
+            this.pCerrar.Controls.Add(this.lblCerrar);
+            this.pCerrar.Location = new System.Drawing.Point(610, 12);
+            this.pCerrar.Name = "pCerrar";
+            this.pCerrar.Size = new System.Drawing.Size(81, 37);
+            this.pCerrar.TabIndex = 1;
+            this.pCerrar.Click += new System.EventHandler(this.pCerrar_Click);
+            this.pCerrar.MouseHover += new System.EventHandler(this.pCerrar_MouseHover);
+            // 
+            // lblCerrar
+            // 
+            this.lblCerrar.ActiveLinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.AutoSize = true;
+            this.lblCerrar.BackColor = System.Drawing.Color.Red;
+            this.lblCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCerrar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblCerrar.LinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.Location = new System.Drawing.Point(16, 11);
+            this.lblCerrar.Name = "lblCerrar";
+            this.lblCerrar.Size = new System.Drawing.Size(47, 15);
+            this.lblCerrar.TabIndex = 0;
+            this.lblCerrar.TabStop = true;
+            this.lblCerrar.Text = "Cerrar";
+            this.lblCerrar.VisitedLinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblCerrar_LinkClicked);
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.BackColor = System.Drawing.Color.DarkKhaki;
+            this.gbFiltros.Controls.Add(this.btnBuscar);
+            this.gbFiltros.Controls.Add(this.btnNuevaBusqueda);
+            this.gbFiltros.Controls.Add(this.txtBusqueda);
+            this.gbFiltros.Controls.Add(this.lblDigiteSuBusqueda);
+            this.gbFiltros.Controls.Add(this.comboBox1);
+            this.gbFiltros.Controls.Add(this.lblFiltrarpor);
+            this.gbFiltros.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbFiltros.ForeColor = System.Drawing.Color.Black;
+            this.gbFiltros.Location = new System.Drawing.Point(32, 71);
+            this.gbFiltros.Name = "gbFiltros";
+            this.gbFiltros.Size = new System.Drawing.Size(653, 106);
+            this.gbFiltros.TabIndex = 2;
+            this.gbFiltros.TabStop = false;
+            this.gbFiltros.Text = "Filtro de Busqueda";
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.BackgroundColor = System.Drawing.Color.DarkKhaki;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(32, 183);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.Size = new System.Drawing.Size(653, 311);
+            this.dgvUsuarios.TabIndex = 3;
+            // 
+            // lblFiltrarpor
+            // 
+            this.lblFiltrarpor.AutoSize = true;
+            this.lblFiltrarpor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiltrarpor.Location = new System.Drawing.Point(25, 32);
+            this.lblFiltrarpor.Name = "lblFiltrarpor";
+            this.lblFiltrarpor.Size = new System.Drawing.Size(62, 15);
+            this.lblFiltrarpor.TabIndex = 4;
+            this.lblFiltrarpor.Text = "Filtrar por:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Codigo",
+            "Nombre Usuario"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 23);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // lblDigiteSuBusqueda
+            // 
+            this.lblDigiteSuBusqueda.AutoSize = true;
+            this.lblDigiteSuBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigiteSuBusqueda.Location = new System.Drawing.Point(232, 32);
+            this.lblDigiteSuBusqueda.Name = "lblDigiteSuBusqueda";
+            this.lblDigiteSuBusqueda.Size = new System.Drawing.Size(117, 15);
+            this.lblDigiteSuBusqueda.TabIndex = 6;
+            this.lblDigiteSuBusqueda.Text = "Digite su Busqueda:";
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBusqueda.Location = new System.Drawing.Point(235, 50);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(261, 21);
+            this.txtBusqueda.TabIndex = 7;
+            // 
+            // btnNuevaBusqueda
+            // 
+            this.btnNuevaBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevaBusqueda.Location = new System.Drawing.Point(507, 33);
+            this.btnNuevaBusqueda.Name = "btnNuevaBusqueda";
+            this.btnNuevaBusqueda.Size = new System.Drawing.Size(134, 23);
+            this.btnNuevaBusqueda.TabIndex = 8;
+            this.btnNuevaBusqueda.Text = "Nueva Busqueda";
+            this.btnNuevaBusqueda.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(507, 58);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(134, 24);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // frmConsultaUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(714, 506);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.gbFiltros);
+            this.Controls.Add(this.pCerrar);
+            this.Controls.Add(this.pOcultar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmConsultaUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta Usuarios";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmConsultaUsuarios_MouseDown);
+            this.MouseHover += new System.EventHandler(this.frmConsultaUsuarios_MouseHover);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmConsultaUsuarios_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmConsultaUsuarios_MouseUp);
+            this.pOcultar.ResumeLayout(false);
+            this.pOcultar.PerformLayout();
+            this.pCerrar.ResumeLayout(false);
+            this.pCerrar.PerformLayout();
+            this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pOcultar;
+        private System.Windows.Forms.LinkLabel lblOcultar;
+        private System.Windows.Forms.Panel pCerrar;
+        private System.Windows.Forms.LinkLabel lblCerrar;
+        private System.Windows.Forms.GroupBox gbFiltros;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblFiltrarpor;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.Label lblDigiteSuBusqueda;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnNuevaBusqueda;
+    }
+}
---
+++ SisFerretero/frmConsultaUsuarios.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SisFerretero
+{
+    public partial class frmConsultaUsuarios : Form
+    {
+        public frmConsultaUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        // eventos para cambiar la forma del puntero
+        private void pOcultar_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+        private void pCerrar_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+        private void frmConsultaUsuarios_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+        // fin eventos para cambiar la forma del puntero
+
+        // eventos para cerrar y minimizar la ventana
+        private void lblOcultar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void lblCerrar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que desea cerrar la ventana?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void pOcultar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void pCerrar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que desea cerrar la ventana?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        // fin eventos para cerrar y minimizar la ventana
+
+
+        // eventos para mover la ventana
+        private bool mousePresionado;
+        private Point posicionActual;
+        private void frmConsultaUsuarios_MouseDown(object sender, MouseEventArgs e)
+        {
+            posicionActual = new Point(Cursor.Position.X - Location.X, Cursor.Position.Y - Location.Y);
+            mousePresionado = true;
+        }
+        private void frmConsultaUsuarios_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousePresionado = false;
+        }
+        private void frmConsultaUsuarios_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousePresionado)
+            {
+                this.Location = new Point(Cursor.Position.X - posicionActual.X, Cursor.Position.Y - posicionActual.Y);
+            }
+        }
+        // fin eventos para mover la ventana
+    }
+}
