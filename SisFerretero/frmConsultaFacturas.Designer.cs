a604531bde68f8f99960b56cac4b0523ab48c803
albertraza <arbert.jr@gmail.com>
2016-07-14 11:31:33 -0400

    add new form for searching facturas

---
+++ SisFerretero/frmConsultaFacturas.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace SisFerretero
+{
+    partial class frmConsultaFacturas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsultaFacturas));
+            this.pOcultar = new System.Windows.Forms.Panel();
+            this.lblOcultar = new System.Windows.Forms.LinkLabel();
+            this.pCerrar = new System.Windows.Forms.Panel();
+            this.lblCerrar = new System.Windows.Forms.LinkLabel();
+            this.gbFiltro = new System.Windows.Forms.GroupBox();
+            this.dgvFacturas = new System.Windows.Forms.DataGridView();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.cbFiltro = new System.Windows.Forms.ComboBox();
+            this.lblBusqueda = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBusqueda = new System.Windows.Forms.MaskedTextBox();
+            this.pOcultar.SuspendLayout();
+            this.pCerrar.SuspendLayout();
+            this.gbFiltro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pOcultar
+            // 
+            this.pOcultar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pOcultar.Controls.Add(this.lblOcultar);
+            this.pOcultar.Location = new System.Drawing.Point(761, 12);
+            this.pOcultar.Name = "pOcultar";
+            this.pOcultar.Size = new System.Drawing.Size(71, 37);
+            this.pOcultar.TabIndex = 0;
+            // 
+            // lblOcultar
+            // 
+            this.lblOcultar.ActiveLinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.AutoSize = true;
+            this.lblOcultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOcultar.ForeColor = System.Drawing.Color.Black;
+            this.lblOcultar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblOcultar.LinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.Location = new System.Drawing.Point(7, 11);
+            this.lblOcultar.Name = "lblOcultar";
+            this.lblOcultar.Size = new System.Drawing.Size(57, 16);
+            this.lblOcultar.TabIndex = 0;
+            this.lblOcultar.TabStop = true;
+            this.lblOcultar.Text = "Ocultar";
+            this.lblOcultar.VisitedLinkColor = System.Drawing.Color.Black;
+            // 
+            // pCerrar
+            // 
+            this.pCerrar.BackColor = System.Drawing.Color.Red;
+            this.pCerrar.Controls.Add(this.lblCerrar);
+            this.pCerrar.Location = new System.Drawing.Point(838, 12);
+            this.pCerrar.Name = "pCerrar";
+            this.pCerrar.Size = new System.Drawing.Size(71, 37);
+            this.pCerrar.TabIndex = 1;
+            // 
+            // lblCerrar
+            // 
+            this.lblCerrar.ActiveLinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.AutoSize = true;
+            this.lblCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCerrar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblCerrar.LinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.Location = new System.Drawing.Point(9, 11);
+            this.lblCerrar.Name = "lblCerrar";
+            this.lblCerrar.Size = new System.Drawing.Size(51, 16);
+            this.lblCerrar.TabIndex = 0;
+            this.lblCerrar.TabStop = true;
+            this.lblCerrar.Text = "Cerrar";
+            this.lblCerrar.VisitedLinkColor = System.Drawing.Color.WhiteSmoke;
+            // 
+            // gbFiltro
+            // 
+            this.gbFiltro.BackColor = System.Drawing.Color.LightGray;
+            this.gbFiltro.Controls.Add(this.txtBusqueda);
+            this.gbFiltro.Controls.Add(this.btnBuscar);
+            this.gbFiltro.Controls.Add(this.btnLimpiar);
+            this.gbFiltro.Controls.Add(this.lblBusqueda);
+            this.gbFiltro.Controls.Add(this.cbFiltro);
+            this.gbFiltro.Controls.Add(this.lblFiltro);
+            this.gbFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbFiltro.Location = new System.Drawing.Point(36, 72);
+            this.gbFiltro.Name = "gbFiltro";
+            this.gbFiltro.Size = new System.Drawing.Size(723, 103);
+            this.gbFiltro.TabIndex = 2;
+            this.gbFiltro.TabStop = false;
+            this.gbFiltro.Text = "Filtro de Busqueda";
+            // 
+            // dgvFacturas
+            // 
+            this.dgvFacturas.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dgvFacturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFacturas.Location = new System.Drawing.Point(36, 181);
+            this.dgvFacturas.Name = "dgvFacturas";
+            this.dgvFacturas.Size = new System.Drawing.Size(875, 321);
+            this.dgvFacturas.TabIndex = 0;
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiltro.Location = new System.Drawing.Point(31, 31);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(62, 15);
+            this.lblFiltro.TabIndex = 0;
+            this.lblFiltro.Text = "Filtrar por:";
+            // 
+            // cbFiltro
+            // 
+            this.cbFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFiltro.FormattingEnabled = true;
+            this.cbFiltro.Items.AddRange(new object[] {
+            "Codigo Factura",
+            "Cedula Cliente",
+            "Nombre Cliente",
+            "Fecha Compra"});
+            this.cbFiltro.Location = new System.Drawing.Point(34, 49);
+            this.cbFiltro.Name = "cbFiltro";
+            this.cbFiltro.Size = new System.Drawing.Size(154, 23);
+            this.cbFiltro.TabIndex = 1;
+            // 
+            // lblBusqueda
+            // 
+            this.lblBusqueda.AutoSize = true;
+            this.lblBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBusqueda.Location = new System.Drawing.Point(206, 31);
+            this.lblBusqueda.Name = "lblBusqueda";
+            this.lblBusqueda.Size = new System.Drawing.Size(117, 15);
+            this.lblBusqueda.TabIndex = 3;
+            this.lblBusqueda.Text = "Digite su Busqueda:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(534, 31);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(129, 25);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Nueva Busqueda";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(534, 59);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(129, 25);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBusqueda.Location = new System.Drawing.Point(209, 49);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(319, 21);
+            this.txtBusqueda.TabIndex = 6;
+            // 
+            // frmConsultaFacturas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(943, 514);
+            this.Controls.Add(this.dgvFacturas);
+            this.Controls.Add(this.gbFiltro);
+            this.Controls.Add(this.pCerrar);
+            this.Controls.Add(this.pOcultar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmConsultaFacturas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta Facturas";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.pOcultar.ResumeLayout(false);
+            this.pOcultar.PerformLayout();
+            this.pCerrar.ResumeLayout(false);
+            this.pCerrar.PerformLayout();
+            this.gbFiltro.ResumeLayout(false);
+            this.gbFiltro.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pOcultar;
+        private System.Windows.Forms.LinkLabel lblOcultar;
+        private System.Windows.Forms.Panel pCerrar;
+        private System.Windows.Forms.LinkLabel lblCerrar;
+        private System.Windows.Forms.GroupBox gbFiltro;
+        private System.Windows.Forms.ComboBox cbFiltro;
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.DataGridView dgvFacturas;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblBusqueda;
+        private System.Windows.Forms.MaskedTextBox txtBusqueda;
+    }
+}
