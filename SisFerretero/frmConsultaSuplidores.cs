2203924313c06c137263a7dfa097cad51aa7d878
albertraza <arbert.jr@gmail.com>
2016-08-14 11:05:48 -0400

    changed naming for consulta suplidores

---
+++ SisFerretero/frmConsultaSuplidores.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SisFerretero
+{
+    public partial class frmConsultaSuplidores : Form
+    {
+        // propiedad para detectar si se ha abierto desde el menu principal
+        public bool menu { get; set; }
+
+        public frmConsultaSuplidores()
+        {
+            InitializeComponent();
+        }
+
+        // evento para buscar los suplidores
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cbFiltro.Text == "Codigo")
+                {
+                    dgvSuplidores.DataSource = baseSuplidores.searchSuplidores(txtBusqueda.Text, "", "");
+                }
+                else if (cbFiltro.Text == "Nombre")
+                {
+                    dgvSuplidores.DataSource = baseSuplidores.searchSuplidores("", txtBusqueda.Text, "");
+                }
+                else if (cbFiltro.Text == "Telefono")
+                {
+                    dgvSuplidores.DataSource = baseSuplidores.searchSuplidores("", "", txtBusqueda.Text);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Consultaempleados_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvSuplidores.DataSource = baseSuplidores.listAllSuplidores();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // evento para cambiar el tipo de mouse a utilizar
+        private void pCerrar_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+        private void frmConsultaSuplidores_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+
+        // evento que valida la entrada en el recuadro busqueda
+        private void txtBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            if(cbFiltro.Text == "Codigo")
+            {
+                int compare;
+                if(!int.TryParse(txtBusqueda.Text, out compare))
+                {
+                    txtBusqueda.Clear();
+                }
+            }
+        }
+
+        // evento para limpiar la busqueda
+        private void btnNuevaBusqueda_Click(object sender, EventArgs e)
+        {
+            cbFiltro.SelectedIndex = -1;
+            txtBusqueda.Mask = "";
+            txtBusqueda.Clear();
+            txtBusqueda.Select();
+            try
+            {
+                dgvSuplidores.DataSource = baseSuplidores.listAllSuplidores();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // evento para validar la entrada de texto
+        private void cbFiltro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbFiltro.Text == "Telefono")
+            {
+                txtBusqueda.Mask = "(000)000-0000";
+                txtBusqueda.Clear();
+                txtBusqueda.Select();
+            }
+            else
+            {
+                txtBusqueda.Mask = "";
+                txtBusqueda.Clear();
+                txtBusqueda.Select();
+            }
+        }
+
+        // eventos para cerrar la ventana
+        private void pCerrar_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Esta seguro que desea cerrar la ventana", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void lblCerrar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que desea cerrar la ventana", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        // propiedad para guardar la info del suplidor
+        public baseSuplidores pSuplidor;
+
+        // evento que obtiene la info del suplidor
+        private void dgvSuplidores_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (!menu)
+            {
+                if(baseSuplidores.getSuplidores(Convert.ToInt32(dgvSuplidores.CurrentRow.Cells[0].Value)) != null)
+                {
+                    pSuplidor = baseSuplidores.getSuplidores(Convert.ToInt32(dgvSuplidores.CurrentRow.Cells[0].Value));
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("El Suplidor seleccionado no existe", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+}
---
+++ SisFerretero/frmConsultaSuplidores.designer.cs
@@ -0,0 +1,202 @@
+﻿namespace SisFerretero
+{
+    partial class frmConsultaSuplidores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsultaSuplidores));
+            this.dgvSuplidores = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnNuevaBusqueda = new System.Windows.Forms.Button();
+            this.cbFiltro = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pCerrar = new System.Windows.Forms.Panel();
+            this.lblCerrar = new System.Windows.Forms.LinkLabel();
+            this.txtBusqueda = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSuplidores)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.pCerrar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvSuplidores
+            // 
+            this.dgvSuplidores.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dgvSuplidores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSuplidores.Location = new System.Drawing.Point(12, 126);
+            this.dgvSuplidores.Name = "dgvSuplidores";
+            this.dgvSuplidores.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvSuplidores.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvSuplidores.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.Yellow;
+            this.dgvSuplidores.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvSuplidores.Size = new System.Drawing.Size(654, 241);
+            this.dgvSuplidores.TabIndex = 4;
+            this.dgvSuplidores.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSuplidores_CellContentClick);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(416, 39);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNuevaBusqueda
+            // 
+            this.btnNuevaBusqueda.Location = new System.Drawing.Point(506, 38);
+            this.btnNuevaBusqueda.Name = "btnNuevaBusqueda";
+            this.btnNuevaBusqueda.Size = new System.Drawing.Size(111, 23);
+            this.btnNuevaBusqueda.TabIndex = 6;
+            this.btnNuevaBusqueda.Text = "Nueva Busqueda";
+            this.btnNuevaBusqueda.UseVisualStyleBackColor = true;
+            this.btnNuevaBusqueda.Click += new System.EventHandler(this.btnNuevaBusqueda_Click);
+            // 
+            // cbFiltro
+            // 
+            this.cbFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFiltro.FormattingEnabled = true;
+            this.cbFiltro.Items.AddRange(new object[] {
+            "Codigo",
+            "Nombre",
+            "Telefono"});
+            this.cbFiltro.Location = new System.Drawing.Point(28, 41);
+            this.cbFiltro.Name = "cbFiltro";
+            this.cbFiltro.Size = new System.Drawing.Size(121, 21);
+            this.cbFiltro.TabIndex = 7;
+            this.cbFiltro.SelectedIndexChanged += new System.EventHandler(this.cbFiltro_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.groupBox1.Controls.Add(this.txtBusqueda);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.cbFiltro);
+            this.groupBox1.Controls.Add(this.btnNuevaBusqueda);
+            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(654, 76);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtro de Busqueda";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Digite su Busqueda";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Filtrar por:";
+            // 
+            // pCerrar
+            // 
+            this.pCerrar.BackColor = System.Drawing.Color.Red;
+            this.pCerrar.Controls.Add(this.lblCerrar);
+            this.pCerrar.Location = new System.Drawing.Point(588, 4);
+            this.pCerrar.Name = "pCerrar";
+            this.pCerrar.Size = new System.Drawing.Size(77, 37);
+            this.pCerrar.TabIndex = 9;
+            this.pCerrar.Click += new System.EventHandler(this.pCerrar_Click);
+            this.pCerrar.MouseHover += new System.EventHandler(this.pCerrar_MouseHover);
+            // 
+            // lblCerrar
+            // 
+            this.lblCerrar.AutoSize = true;
+            this.lblCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCerrar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblCerrar.LinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.Location = new System.Drawing.Point(19, 9);
+            this.lblCerrar.Name = "lblCerrar";
+            this.lblCerrar.Size = new System.Drawing.Size(37, 15);
+            this.lblCerrar.TabIndex = 0;
+            this.lblCerrar.TabStop = true;
+            this.lblCerrar.Text = "Salir";
+            this.lblCerrar.VisitedLinkColor = System.Drawing.Color.White;
+            this.lblCerrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblCerrar_LinkClicked);
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBusqueda.Location = new System.Drawing.Point(171, 42);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(225, 20);
+            this.txtBusqueda.TabIndex = 11;
+            // 
+            // frmConsultaSuplidores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(687, 390);
+            this.Controls.Add(this.pCerrar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvSuplidores);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmConsultaSuplidores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta de Suplidores";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Consultaempleados_Load);
+            this.MouseHover += new System.EventHandler(this.frmConsultaSuplidores_MouseHover);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSuplidores)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.pCerrar.ResumeLayout(false);
+            this.pCerrar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSuplidores;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnNuevaBusqueda;
+        private System.Windows.Forms.ComboBox cbFiltro;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pCerrar;
+        private System.Windows.Forms.LinkLabel lblCerrar;
+        private System.Windows.Forms.MaskedTextBox txtBusqueda;
+    }
+}
