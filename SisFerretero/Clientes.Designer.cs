24049dc4d60af3481f925ee1d4cb14f8b235dedb
albertraza <arbert.jr@gmail.com>
2016-08-15 08:17:54 -0400

    add new desing

--- SisFerretero/Clientes.Designer.cs
+++ SisFerretero/Clientes.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SisFerretero
+﻿namespace Proyecto2
 {
     partial class Clientes
     {
@@ -28,21 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clientes));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnaceptar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.mtbtelefono = new System.Windows.Forms.MaskedTextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.mtbcedula = new System.Windows.Forms.MaskedTextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.txtdireccion = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.txtapellido = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtnombre = new System.Windows.Forms.TextBox();
-            this.txtapellido = new System.Windows.Forms.TextBox();
-            this.txtdireccion = new System.Windows.Forms.TextBox();
-            this.mtbcedula = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btncancelar = new System.Windows.Forms.Button();
-            this.btnaceptar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pCerrar = new System.Windows.Forms.Panel();
+            this.lblCerrar = new System.Windows.Forms.LinkLabel();
+            this.pOcultar = new System.Windows.Forms.Panel();
+            this.lblOcultar = new System.Windows.Forms.LinkLabel();
             this.groupBox1.SuspendLayout();
+            this.pCerrar.SuspendLayout();
+            this.pOcultar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btnaceptar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.mtbtelefono);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.mtbcedula);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtdireccion);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtapellido);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtnombre);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(37, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(521, 274);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacion del Cliente";
+            // 
+            // btnaceptar
+            // 
+            this.btnaceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnaceptar.Location = new System.Drawing.Point(36, 206);
+            this.btnaceptar.Name = "btnaceptar";
+            this.btnaceptar.Size = new System.Drawing.Size(75, 40);
+            this.btnaceptar.TabIndex = 11;
+            this.btnaceptar.Text = "Aceptar";
+            this.btnaceptar.UseVisualStyleBackColor = true;
+            this.btnaceptar.Click += new System.EventHandler(this.btnaceptar_Click_1);
             // 
             // label1
             // 
@@ -54,6 +94,15 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre:";
             // 
+            // mtbtelefono
+            // 
+            this.mtbtelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbtelefono.Location = new System.Drawing.Point(388, 90);
+            this.mtbtelefono.Mask = "(000) 000-0000";
+            this.mtbtelefono.Name = "mtbtelefono";
+            this.mtbtelefono.Size = new System.Drawing.Size(102, 22);
+            this.mtbtelefono.TabIndex = 10;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -64,31 +113,53 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Apellido:";
             // 
+            // mtbcedula
+            // 
+            this.mtbcedula.Location = new System.Drawing.Point(331, 52);
+            this.mtbcedula.Mask = "000-0000000-0";
+            this.mtbcedula.Name = "mtbcedula";
+            this.mtbcedula.Size = new System.Drawing.Size(124, 22);
+            this.mtbcedula.TabIndex = 9;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(33, 164);
+            this.label3.Location = new System.Drawing.Point(33, 130);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Direccion:";
             // 
+            // txtdireccion
+            // 
+            this.txtdireccion.Location = new System.Drawing.Point(107, 127);
+            this.txtdireccion.Name = "txtdireccion";
+            this.txtdireccion.Size = new System.Drawing.Size(383, 22);
+            this.txtdireccion.TabIndex = 8;
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(33, 199);
+            this.label4.Location = new System.Drawing.Point(271, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(111, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "Telefono/Celular:";
             // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(96, 89);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(133, 22);
+            this.txtapellido.TabIndex = 6;
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(33, 130);
+            this.label5.Location = new System.Drawing.Point(271, 55);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(54, 16);
             this.label5.TabIndex = 4;
@@ -101,112 +172,119 @@
             this.txtnombre.Size = new System.Drawing.Size(133, 22);
             this.txtnombre.TabIndex = 5;
             // 
-            // txtapellido
+            // label6
             // 
-            this.txtapellido.Location = new System.Drawing.Point(96, 89);
-            this.txtapellido.Name = "txtapellido";
-            this.txtapellido.Size = new System.Drawing.Size(133, 22);
-            this.txtapellido.TabIndex = 6;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(262, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Clientes";
             // 
-            // txtdireccion
+            // pCerrar
             // 
-            this.txtdireccion.Location = new System.Drawing.Point(104, 161);
-            this.txtdireccion.Name = "txtdireccion";
-            this.txtdireccion.Size = new System.Drawing.Size(245, 22);
-            this.txtdireccion.TabIndex = 8;
+            this.pCerrar.BackColor = System.Drawing.Color.Red;
+            this.pCerrar.Controls.Add(this.lblCerrar);
+            this.pCerrar.Location = new System.Drawing.Point(479, 25);
+            this.pCerrar.Name = "pCerrar";
+            this.pCerrar.Size = new System.Drawing.Size(81, 37);
+            this.pCerrar.TabIndex = 15;
             // 
-            // mtbcedula
+            // lblCerrar
             // 
-            this.mtbcedula.Location = new System.Drawing.Point(93, 124);
-            this.mtbcedula.Mask = "000-0000000-0";
-            this.mtbcedula.Name = "mtbcedula";
-            this.mtbcedula.Size = new System.Drawing.Size(107, 22);
-            this.mtbcedula.TabIndex = 9;
+            this.lblCerrar.ActiveLinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.AutoSize = true;
+            this.lblCerrar.BackColor = System.Drawing.Color.Red;
+            this.lblCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCerrar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblCerrar.LinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.Location = new System.Drawing.Point(16, 11);
+            this.lblCerrar.Name = "lblCerrar";
+            this.lblCerrar.Size = new System.Drawing.Size(47, 15);
+            this.lblCerrar.TabIndex = 0;
+            this.lblCerrar.TabStop = true;
+            this.lblCerrar.Text = "Cerrar";
+            this.lblCerrar.VisitedLinkColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCerrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblCerrar_LinkClicked);
             // 
-            // maskedTextBox2
+            // pOcultar
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(150, 196);
-            this.maskedTextBox2.Mask = "(000) 000-0000";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(79, 22);
-            this.maskedTextBox2.TabIndex = 10;
+            this.pOcultar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pOcultar.Controls.Add(this.lblOcultar);
+            this.pOcultar.Location = new System.Drawing.Point(392, 25);
+            this.pOcultar.Name = "pOcultar";
+            this.pOcultar.Size = new System.Drawing.Size(81, 37);
+            this.pOcultar.TabIndex = 14;
             // 
-            // groupBox1
+            // lblOcultar
             // 
-            this.groupBox1.Controls.Add(this.btncancelar);
-            this.groupBox1.Controls.Add(this.btnaceptar);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.maskedTextBox2);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.mtbcedula);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.txtdireccion);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.txtapellido);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.txtnombre);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(27, 32);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(399, 320);
-            this.groupBox1.TabIndex = 11;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Nuevo Cliente";
-            // 
-            // btncancelar
-            // 
-            this.btncancelar.Location = new System.Drawing.Point(182, 247);
-            this.btncancelar.Name = "btncancelar";
-            this.btncancelar.Size = new System.Drawing.Size(75, 40);
-            this.btncancelar.TabIndex = 12;
-            this.btncancelar.Text = "Cancelar";
-            this.btncancelar.UseVisualStyleBackColor = true;
-            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
-            // 
-            // btnaceptar
-            // 
-            this.btnaceptar.Location = new System.Drawing.Point(69, 247);
-            this.btnaceptar.Name = "btnaceptar";
-            this.btnaceptar.Size = new System.Drawing.Size(75, 40);
-            this.btnaceptar.TabIndex = 11;
-            this.btnaceptar.Text = "Aceptar";
-            this.btnaceptar.UseVisualStyleBackColor = true;
-            this.btnaceptar.Click += new System.EventHandler(this.btnaceptar_Click);
+            this.lblOcultar.ActiveLinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.AutoSize = true;
+            this.lblOcultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOcultar.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lblOcultar.LinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.Location = new System.Drawing.Point(13, 11);
+            this.lblOcultar.Name = "lblOcultar";
+            this.lblOcultar.Size = new System.Drawing.Size(53, 15);
+            this.lblOcultar.TabIndex = 0;
+            this.lblOcultar.TabStop = true;
+            this.lblOcultar.Text = "Ocultar";
+            this.lblOcultar.VisitedLinkColor = System.Drawing.Color.Black;
+            this.lblOcultar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblOcultar_LinkClicked);
             // 
             // Clientes
             // 
+            this.AcceptButton = this.btnaceptar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(465, 385);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.CancelButton = this.lblCerrar;
+            this.ClientSize = new System.Drawing.Size(602, 424);
+            this.Controls.Add(this.pCerrar);
+            this.Controls.Add(this.pOcultar);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Clientes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Clientes";
+            this.TransparencyKey = System.Drawing.Color.White;
             this.Load += new System.EventHandler(this.Clientes_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.pCerrar.ResumeLayout(false);
+            this.pCerrar.PerformLayout();
+            this.pOcultar.ResumeLayout(false);
+            this.pOcultar.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnaceptar;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mtbtelefono;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox mtbcedula;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtdireccion;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtapellido;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtnombre;
-        private System.Windows.Forms.TextBox txtapellido;
-        private System.Windows.Forms.TextBox txtdireccion;
-        private System.Windows.Forms.MaskedTextBox mtbcedula;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btncancelar;
-        private System.Windows.Forms.Button btnaceptar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel pCerrar;
+        private System.Windows.Forms.LinkLabel lblCerrar;
+        private System.Windows.Forms.Panel pOcultar;
+        private System.Windows.Forms.LinkLabel lblOcultar;
+
     }
 }
--- SisFerretero/Clientes.cs
+++ SisFerretero/Clientes.cs
@@ -5,10 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
-namespace SisFerretero
+namespace Proyecto2
 {
     public partial class Clientes : Form
     {
@@ -23,14 +23,82 @@ namespace SisFerretero
             txtapellido.Focus();
         }
 
-        private void btnaceptar_Click(object sender, EventArgs e)
+        private void btnaceptar_Click_1(object sender, EventArgs e)
         {
+             if (txtnombre.Text == string.Empty || txtapellido.Text == string.Empty || mtbcedula.Text == string.Empty || txtdireccion.Text == string.Empty || mtbtelefono.Text == string.Empty)
+            {
+                MessageBox.Show("Ingrese todos los datos.");
+                txtnombre.Select();
+            }             
+            else
+                try
+                {
+                    SqlConnection con = new SqlConnection(@" ");
 
-        }
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = "INSERT INTO Clientes (Nombre, Apellido, Cedula, Direccion, Telefono) VALUES ('" + txtnombre.Text + "','" + txtapellido.Text + "','" + mtbcedula.Text + "','" + txtdireccion.Text + "','" + mtbtelefono.Text + "')";
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Cliente guardado exitosamente.");
+                    con.Close();
+                    txtnombre.Clear();
+                    txtapellido.Clear();
+                    mtbcedula.Clear();
+                    txtdireccion.Clear();
+                    mtbtelefono.Clear();
+                    txtnombre.Select();                   
 
-        private void btncancelar_Click(object sender, EventArgs e)
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                } 
+        }       
+
+        private void lblOcultar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             this.Hide();
+        }
+
+        private void lblCerrar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void llblbuscar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@" ");
+
+                con.Open();
+                string buscar = "Select * From Clientes where Cedula='" + mtbcedula.Text + "'";
+                SqlCommand cmd = new SqlCommand(buscar, con);
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(buscar, con);
+                da.Fill(dt);
+
+                string nombre = dt.Rows[0]["Nombre"].ToString();
+                string apellido = dt.Rows[0]["Apellido"].ToString();
+                string cedula = dt.Rows[0]["Cedula"].ToString();
+                string direccion = dt.Rows[0]["Direccion"].ToString();
+                string telefono = dt.Rows[0]["Telefono"].ToString();
+                
+                
+                txtnombre.Text = nombre;
+                txtapellido.Text = apellido;
+                mtbcedula.Text = cedula;
+                txtdireccion.Text = direccion;
+                mtbtelefono.Text = telefono;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
